e5eb3055f8c1a580b522d24152f94b3e2376fa3b
caoye <mailcaoye@163.com>
2017-12-14 09:03:05 +0800

    add log4j2

---
+++ src/com/hisense/basic/util/LogUtil.java
@@ -0,0 +1,16 @@
+package com.hisense.basic.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogUtil {
+	private static final Logger logger=LogManager.getLogger("mylog");
+	public static void main(String[] args) {
+		logger.trace("trace level");
+		logger.debug("debug level");
+		logger.info("info level");
+		logger.warn("warn level");
+		logger.error("error level");
+		logger.fatal("fatal level");
+	}
+}
